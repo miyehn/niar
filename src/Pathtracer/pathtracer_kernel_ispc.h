9e4da79967b8cc52e311cccc3e6f583ab3d8314e
miyehn <rainduym@gmail.com>
2020-11-14 03:27:55 +0800

    wip porting functions to ispc

--- src/Pathtracer/pathtracer_kernel_ispc.h
+++ src/Pathtracer/pathtracer_kernel_ispc.h
@@ -25,6 +25,21 @@ namespace ispc { /* namespace */
 #endif
 #endif
 
+#ifndef __ISPC_STRUCT_Triangle__
+#define __ISPC_STRUCT_Triangle__
+struct Triangle {
+};
+#endif
+
+#ifndef __ISPC_STRUCT_vec3__
+#define __ISPC_STRUCT_vec3__
+struct vec3 {
+    float x;
+    float y;
+    float z;
+};
+#endif
+
 
 ///////////////////////////////////////////////////////////////////////////
 // Functions exported from ispc code
@@ -32,6 +47,7 @@ namespace ispc { /* namespace */
 #if defined(__cplusplus) && (! defined(__ISPC_NO_EXTERN_C) || !__ISPC_NO_EXTERN_C )
 extern "C" {
 #endif // __cplusplus
+    extern void raytrace_scene_ispc(struct Triangle * triangles, int32_t num_triangles, struct vec3 * output, int32_t width, int32_t height);
     extern void test_kernel(int32_t * input, int32_t * output, int32_t len);
 #if defined(__cplusplus) && (! defined(__ISPC_NO_EXTERN_C) || !__ISPC_NO_EXTERN_C )
 } /* end extern C */
