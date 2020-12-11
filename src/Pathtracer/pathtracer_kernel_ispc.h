bc5b193687580a2520d01d43c7ca3c1001cc285b
miyehn <rainduym@gmail.com>
2020-12-12 05:10:05 +0800

    buggy wip

--- src/Pathtracer/pathtracer_kernel_ispc.h
+++ src/Pathtracer/pathtracer_kernel_ispc.h
@@ -20,7 +20,8 @@ namespace ispc { /* namespace */
 #define __ISPC_ENUM_BSDF_t__
 enum BSDF_t {
     Diffuse = 0,
-    Mirror = 1 
+    Mirror = 1,
+    Glass = 2 
 };
 #endif
 
@@ -95,7 +96,7 @@ struct BSDF {
 #if defined(__cplusplus) && (! defined(__ISPC_NO_EXTERN_C) || !__ISPC_NO_EXTERN_C )
 extern "C" {
 #endif // __cplusplus
-    extern void raytrace_scene_ispc(struct Camera * camera, float * pixel_offsets, uint32_t num_offsets, struct Triangle * triangles, struct BSDF * bsdfs, uint32_t * area_light_indices, uint32_t num_triangles, uint32_t num_area_lights, uint8_t * output, uint32_t width, uint32_t height, uint32_t max_ray_depth, float rr_threshold, bool use_direct_light, uint32_t area_light_samples);
+    extern void raytrace_scene_ispc(struct Camera * camera, float * pixel_offsets, uint32_t num_offsets, struct Triangle * triangles, struct BSDF * bsdfs, uint32_t * area_light_indices, uint32_t num_triangles, uint32_t num_area_lights, uint8_t * output, uint32_t width, uint32_t height, uint32_t num_threads, uint32_t max_ray_depth, float rr_threshold, bool use_direct_light, uint32_t area_light_samples);
 #if defined(__cplusplus) && (! defined(__ISPC_NO_EXTERN_C) || !__ISPC_NO_EXTERN_C )
 } /* end extern C */
 #endif // __cplusplus
