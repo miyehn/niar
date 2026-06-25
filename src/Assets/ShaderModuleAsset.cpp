49322cc46bcbaf469d626a9e9200e35767320408
miyehn <rainduym@gmail.com>
2026-06-25 12:40:49 -0700

    bindless self test

--- src/Assets/ShaderModuleAsset.cpp
+++ src/Assets/ShaderModuleAsset.cpp
@@ -32,6 +32,7 @@ ShaderModuleDef ShaderModuleAsset::_shaderModuleDefs[] = {
 	{ "shaders/sky_transmittance_lut.comp", "main", SS_Compute },
 	{ "shaders/sky_view_lut.comp",          "main", SS_Compute },
 	{ "shaders/rtgi_generate.comp",         "main", SS_Compute },
+	{ "shaders/bindless_self_test.comp",    "main", SS_Compute },
 	{ "shaders/ray_gen.rgen",               "main", SS_RayGen },
 	{ "shaders/ray_chit.rchit",             "main", SS_ClosestHit },
 	{ "shaders/ray_chit2.rchit",            "main", SS_ClosestHit },
