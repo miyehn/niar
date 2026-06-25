12647355d4340461647401f74bf9a27315265d09
miyehn <rainduym@gmail.com>
2026-06-24 22:11:13 -0700

    bindless resources header

--- src/Render/BindlessResources.h
+++ src/Render/BindlessResources.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "Render/Vulkan/DescriptorSet.h"
+
+#include <array>
 #include <cstdint>
-#include <optional>
+#include <vector>
 #include <vulkan/vulkan.h>
 
 constexpr uint32_t MAX_BINDLESS_TEXTURES_2D = 1024;
@@ -16,6 +19,43 @@ struct BindlessTexture2DHandle
 struct BindlessTexture2DInfo
 {
 	bool registerTexture = false;
-	// nullopt uses SamplerCache::defaultInfo() when registration is implemented.
-	std::optional<VkSamplerCreateInfo> samplerInfo;
+	VkSamplerCreateInfo samplerInfo;
+};
+
+class BindlessResources
+{
+public:
+	static BindlessResources* Instance;
+
+	BindlessResources() = default;
+	BindlessResources(const BindlessResources&) = delete;
+	BindlessResources& operator=(const BindlessResources&) = delete;
+
+	void init();
+	void release();
+
+	BindlessTexture2DHandle addTexture2D(
+		VkImageView imageView,
+		const VkSamplerCreateInfo& samplerInfo);
+	void removeTexture2D(BindlessTexture2DHandle handle);
+
+	// Returns the shader-visible index. Invalid or stale handles are errors.
+	uint32_t validate(BindlessTexture2DHandle handle) const;
+
+	const DescriptorSet& descriptorSet() const { return bindlessDescriptorSet; }
+	const DescriptorSetLayout& layout() const { return bindlessSetLayout; }
+
+private:
+	struct Texture2DSlot
+	{
+		uint32_t generation = 0;
+		bool occupied = false;
+	};
+
+	VkDescriptorPool descriptorPool = VK_NULL_HANDLE;
+	DescriptorSetLayout bindlessSetLayout;
+	DescriptorSet bindlessDescriptorSet;
+
+	std::array<Texture2DSlot, MAX_BINDLESS_TEXTURES_2D> texture2DSlots{};
+	std::vector<uint32_t> freeTexture2DSlots;
 };
